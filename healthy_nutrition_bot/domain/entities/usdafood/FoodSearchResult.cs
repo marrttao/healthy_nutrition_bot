cd942f576bc222e4891b7095192e2b6f206f0203
marrttao <marrttaoburberry@gmail.com>
2026-07-06 19:20:09 +0300

    changes with dbcontext added fabctory patt

--- healthy_nutrition_bot/domain/entities/usdafood/FoodSearchResult.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace healthy_nutrition_bot.domain.entities;
-
-public class FoodSearchResult
-{
-    public List<Food> Foods { get; set; } = new List<Food>();
-}
--- healthy_nutrition_bot/domain/entities/usdafood/Nutrient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace healthy_nutrition_bot.domain.entities;
-
-public class Nutrient
-{
-    public string NutrientName { get; set; } = string.Empty;
-    public string UnitName { get; set; } = string.Empty;
-    public float Value { get; set; }
-}
